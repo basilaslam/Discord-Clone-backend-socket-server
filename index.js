2f8839173f065ed896fa988c80143b99f6440dfe
Basil Aslam N P <64439076+basilaslam@users.noreply.github.com>
2023-04-03 17:41:22 +0100

    fix cors

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const dotenv = require('dotenv').config()
 const app = express();
 const PORT = process.env.PORT || 5000;
+var cors = require('cors')
+
+app.use(cors({
+    origin: '*'
+}));
+
 
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
